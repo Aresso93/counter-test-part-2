967225e74918620c5c3532d5df7a9c2e8e531ed9
Aresso93 <135212764+Aresso93@users.noreply.github.com>
2023-11-08 17:41:50 +0100

    /

--- src/components/wrapper.control-counter.tsx
+++ src/components/wrapper.control-counter.tsx
@@ -19,9 +19,7 @@ export function WrapperControlCounter(props:ControlProps){
     title={'Controlli'}>
     <ControlCounter
     actions={props.actions}
-    >
-        
-    </ControlCounter>
+    />   
     </Wrapper>
     </>
     )
